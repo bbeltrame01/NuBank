7d71fc534317160f830c408fbf9a2bcd0f283990
Bruno Beltrame <bruno.beltrame@systempro.com.br>
2019-09-13 23:22:08 -0300

    Menu / Effects / Transitions

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
-import { Container } from './styles';
 
-export default function Menu() {
+//react-native-qrcode-svg: https://www.npmjs.com/package/react-native-qrcode-svg
+import QRCode from 'react-native-qrcode-svg'; 
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { 
+    Container, Code, Nav, 
+    NavItem, NavText, 
+    SignOutButton, SignOutButtonText,
+} from './styles';
+
+export default function Menu({ translateY }) {
     return(
-        <Container />
+        <Container style={{
+            opacity: translateY.interpolate({
+              inputRange: [0, 150],
+              outputRange: [0, 1],
+            }),
+          }}>
+            <Code>
+                <QRCode 
+                    value="https://github.com/bbeltrame01/myNuBank"
+                    size={70}
+                    backgroundColor="#FFF"
+                    color="#8B10AE"
+                />
+            </Code>
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#FFF" />
+                    <NavText>Me Ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#FFF" />
+                    <NavText>Meu Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#FFF" />
+                    <NavText>Configurar Cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#FFF" />
+                    <NavText>Configurar App</NavText>
+                </NavItem>
+            </Nav>
+            <SignOutButton onPress={() => {}}>
+                <SignOutButtonText>SAIR DO APP</SignOutButtonText>
+            </SignOutButton>
+        </Container>
     )
 }
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler'; //https://kmagiera.github.io/react-native-gesture-handler/docs/getting-started.html
+
 import Header from '~/components/Header';
 
 import Tabs from '~/components/Tabs';
@@ -15,28 +18,85 @@ import {
 } from './styles';
 
 export default function Main(){
+  let offset = 0;
+const translateY = new Animated.Value(0);
+
+const animatedEvent = Animated.event(
+  [
+    {
+      nativeEvent: {
+        translationY: translateY,
+      },
+    },
+  ],
+  { useNativeDriver: true },
+); 
+
+function onHandlerStateChanged(event) {
+  if (event.nativeEvent.oldState === State.ACTIVE){
+    let opened = false;
+    const { translationY } = event.nativeEvent;
+
+    offset += translationY; 
+
+    if (translationY >= 100){ // maior que 100 = Aberto
+      opened = true;      
+    } else {
+      translateY.setValue(offset);
+      translateY.setOffset(0);
+      offset = 0;
+    }
+
+    Animated.timing(translateY, {
+      toValue: opened ? 395 : 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start(() => {
+      offset = opened ? 395 : 0;
+      translateY.setOffset(offset);
+      translateY.setValue(0);
+    });
+  }
+}
   return(
     <Container>
       <Header />
+
       <Content>
-      <Menu />
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" size={28} color="#666" />
-            <Icon name="visibility-off" size={28} color="#666" />
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo Disponível</Title>
-            <Description>R$ -1.850,53</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Compra no valor de R$ 2,50 no cartão negada. Cartão de crédito bloqueado. 
-            </Annotation>
-          </CardFooter>
-        </Card>
+
+      <Menu translateY={translateY} />
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [0, 400],
+                outputRange: [0, 315],
+                extrapolate: "clamp",
+              }), 
+            }],
+          }}
+          >
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#666" />
+              <Icon name="visibility-off" size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo Disponível</Title>
+              <Description>R$ -1.850,53</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Compra no valor de R$ 2,50 no cartão negada. Cartão de crédito bloqueado. 
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs />
+
+      <Tabs translateY={translateY} />
     </Container>
   );
 }
